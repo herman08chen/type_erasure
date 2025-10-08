47f1cc361ea8ee7b5a712aea7714f5b76ef4139c
herman08chen <herman08chen@gmail.com>
2025-10-07 22:19:26 -0400

    d

--- main.cpp
+++ main.cpp
@@ -1,101 +1,73 @@
+#include <functional>
 #include <iostream>
 #include <type_traits>
 #include <memory>
 
-
-template<std::size_t N, std::size_t alignment = 8>
-class buffer {
-    alignas(alignment) std::array<std::byte, N> _data;
-public:
-    template<class T>
-    buffer(const T& d) {
-        static_assert(sizeof(T) <= N);
-        new (_data.data()) T{d};
-    }
-    template<class T>
-    buffer(T&& d) {
-        static_assert(sizeof(T) <= N);
-        new (_data.data()) T{std::move(d)};
-    }
-    template<class T>
-    T& get() const noexcept {
-        return *static_cast<T*>(_data.data());
-    }
-};
-
-template<class T>
-concept Stateless = std::is_empty_v<T>;
-
 template<class T, class vtable_t, std::size_t _size = 24, std::size_t _alignment = 8>
 class local_buffer_t {
     static constexpr std::size_t buffer_size = _size - sizeof(T*);
-    T* _data;
-    alignas(_alignment) std::byte _buffer[buffer_size];
-
-    template<Stateless Deleter>
-    struct state_0_t  {
-        [[no_unique_address]] Deleter d;
-        std::byte padding[buffer_size - sizeof(vtable_t*)];
-        vtable_t* vtable;
-    };
-    template<class Deleter>
-    struct state_0_t  {
-        Deleter d;
-        std::byte padding[buffer_size - sizeof(vtable_t*) - sizeof(Deleter)];
-        vtable_t* vtable;
-    };
-    template<Stateless Deleter>
-    struct state_1_t  {
-        [[no_unique_address]] Deleter d;
-        std::byte padding[buffer_size - sizeof(vtable_t*)];
-        vtable_t vtable;
-    };
-    template<class Deleter>
-    struct state_1_t  {
-        Deleter d;
-        std::byte padding[buffer_size - sizeof(vtable_t*) - sizeof(Deleter)];
-        vtable_t vtable;
-    };
-
     static constexpr std::ptrdiff_t one = 1;
     static constexpr std::ptrdiff_t not_one = ~one;
 
+    T* _ptr;
+    alignas(_alignment) std::byte _buffer[buffer_size];
+
     template<class Deleter>
-    local_buffer_t(T* _data, Deleter&& deleter, const vtable_t* vtable) : _data{_data} {
-        static_assert(alignof(T) > 1);
-        if constexpr(sizeof(state_1_t<Deleter>) <= _size - sizeof(T*)) {
-            new (static_cast<void*>(_buffer)) state_1_t<Deleter>(std::forward<Deleter>(deleter), vtable);
-            set_state<1>();
-        }
-        else {
-            new (static_cast<void*>(_buffer)) state_0_t<Deleter>(std::forward<Deleter>(deleter), *vtable);
-            set_state<0>();
-        }
-    }
+    struct state_0_t  {
+        union {
+            [[no_unique_address]] Deleter d;
+            const std::byte padding[buffer_size - sizeof(vtable_t*)];
+        };
+        const vtable_t* vtable;
+    };
+    template<class Deleter>
+    struct state_1_t {
+        union {
+            [[no_unique_address]] Deleter d;
+            const std::byte padding[buffer_size - sizeof(vtable_t)];
+        };
+        const vtable_t vtable;
+    };
+
     template<std::size_t state>
     void set_state() noexcept {
         if constexpr (state == 0) {
-            _data = static_cast<T*>(static_cast<std::ptrdiff_t>(_data) & not_one);
+            _ptr = reinterpret_cast<T*>(reinterpret_cast<std::ptrdiff_t>(_ptr) & not_one);
         }
         else {
-            _data = static_cast<T*>(static_cast<std::ptrdiff_t>(_data) | one);
+            _ptr = reinterpret_cast<T*>(reinterpret_cast<std::ptrdiff_t>(_ptr) | one);
         }
     }
      auto get_state() const noexcept {
-        return static_cast<std::ptrdiff_t>(_data & one);
+        return static_cast<std::size_t>(reinterpret_cast<std::ptrdiff_t>(_ptr) & one);
     }
 public:
-    T* get() const noexcept {
-        return static_cast<T*>(static_cast<std::ptrdiff_t>(_data) & not_one);
-    }
-    const vtable_t& get_vtable() const noexcept {
-        if(get_state == 1) {
-            return static_cast<state_1_t<std::default_delete<T>>*>(_buffer)->vtable;
+    template<class Deleter>
+    local_buffer_t(T* _data, Deleter&& deleter, const vtable_t* vtable) : _ptr{_data} {
+        static_assert(alignof(T) > 1);
+        if constexpr(sizeof(state_1_t<Deleter>) == _size - sizeof(T*)) {
+            new (static_cast<void*>(_buffer)) state_1_t<Deleter>({std::forward<Deleter>(deleter)},  *vtable);
+            set_state<1>();
+            static_assert(offsetof(state_1_t<Deleter>, vtable) == buffer_size - sizeof(vtable_t));
         }
-        return *(static_cast<state_1_t<std::default_delete<T>>*>(_buffer)->vtable);
+        else {
+            static_assert(sizeof(state_0_t<Deleter>) <= buffer_size);
+            new (static_cast<void*>(_buffer)) state_0_t<Deleter>({std::forward<Deleter>(deleter)}, vtable);
+            set_state<0>();
+            static_assert(offsetof(state_0_t<Deleter>, vtable) == buffer_size - sizeof(vtable_t*));
+        }
     }
-    void* get_deleter() const noexcept {
-        return static_cast<void*>(_buffer);
+    T* get() const noexcept {
+        return reinterpret_cast<T*>(reinterpret_cast<std::ptrdiff_t>(_ptr) & not_one);
+    }
+    const vtable_t& vtable() noexcept {
+        if(get_state() == 1) {
+            return static_cast<state_1_t<std::default_delete<T>>*>(static_cast<void*>(static_cast<std::byte*>(_buffer)))->vtable;
+        }
+        return *(static_cast<state_0_t<std::default_delete<T>>*>(static_cast<void*>(static_cast<std::byte*>(_buffer)))->vtable);
+    }
+    void* deleter() noexcept {
+        return reinterpret_cast<void*>(_buffer);
     }
 };
 
@@ -150,26 +122,38 @@ class smartptr_te {
             (*static_cast<Deleter*>(deleter))(ptr);
         }
         static void destructor(void* deleter) {
-            //(*static_cast<Deleter*>(deleter))->~Deleter();
-            delete static_cast<Deleter*>(deleter);
+            static_cast<Deleter*>(deleter)->~Deleter();
+            //delete static_cast<Deleter*>(deleter);
         }
         static constexpr vtable_t vtable{destroy, destructor};
     };
     template<class Deleter>
-    static constexpr vtable_t vtable = vtable_impl<Deleter>::vtable;
+    static constexpr vtable_t vtable = vtable_impl<std::remove_reference_t<Deleter>>::vtable;
 
-    T* _ptr;
+    /*T* _ptr;
     void* _deleter;
-    const vtable_t* _vtable;
+    const vtable_t* _vtable;*/
+
+    local_buffer_t<T, vtable_t> _buffer;
+    T* get() const noexcept {
+        return _buffer.get();
+    }
+    const vtable_t& _vtable() noexcept {
+        return _buffer.vtable();
+    }
+    void* _deleter() noexcept {
+        return _buffer.deleter();
+    }
+
 public:
     template<class Deleter>
-    smartptr_te(T* _ptr, Deleter _deleter) :
-        _ptr(_ptr), _deleter(new Deleter{std::forward<Deleter>(_deleter)}), _vtable(&vtable<Deleter>) {
+    smartptr_te(T* _ptr, Deleter&& _deleter) :
+        _buffer (_ptr, std::forward<Deleter>(_deleter), &vtable<Deleter>) {
         static_assert(std::is_nothrow_move_constructible_v<Deleter>);
     }
     ~smartptr_te() {
-        _vtable->_destroy(_ptr, _deleter);
-        _vtable->_destructor(_deleter);
+        _vtable()._destroy(get(), _deleter());
+        _vtable()._destructor(_deleter());
     }
     template<class Deleter>
     void set_deleter() {
@@ -179,27 +163,27 @@ public:
         _vtable = &vtable<Deleter>;
     }
     auto& operator=(const smartptr_te& rhs) noexcept {
-        if (this != &rhs) {
-            _ptr = rhs._ptr;
-        }
+
     }
     auto& operator=(smartptr_te&& rhs) noexcept {
-        std::swap(_ptr, rhs._ptr);
-        std::swap(_deleter, rhs._deleter);
-        std::swap(_vtable, rhs._vtable);
+
     }
 };
 
 int main() {
-    auto my_deleter = [](void* ptr) {
+    /*auto my_deleter = [](void* ptr) {
         delete static_cast<int*>(ptr);
         std::cout << "destroy called!";
-    };
-    std::cout << sizeof(std::add_pointer_t<void(int*, void*)>) << std::endl;
-    std::cout << sizeof(std::add_pointer_t<void(void*)>) << std::endl;
+    };*/
+    struct my_deleter : decltype([](void* ptr) {
+        delete static_cast<int*>(ptr);
+        std::cout << "destroy called!";
+    }) {};
+
+    std::cout << std::is_empty_v<my_deleter> << std::endl;
 
     smartptr_te p1(new int{42}, std::default_delete<int>{});
-    smartptr_te p2(new int{42}, my_deleter);
+    smartptr_te p2(new int{42}, my_deleter{});
     
     return 0;
 }
